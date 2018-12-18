1e243515fa503fc35f547604bb1fd6362a1066e0
804463592 <804463592@qq.com>
2018-12-18 23:26:26 +0800

    小修改

--- Image_Processing/Image_ProcessingView.cpp
+++ Image_Processing/Image_ProcessingView.cpp
@@ -3703,7 +3703,6 @@ void CImage_ProcessingView::OnHsiHistBalance()
 
 	}
 
-
 	//释放数组内存
 	for (int k = 0; k < 3; k++)
 	{
@@ -3714,8 +3713,6 @@ void CImage_ProcessingView::OnHsiHistBalance()
 		delete[]HsiArr[k];
 	}
 	delete[]HsiArr;
-
-
 
 	Invalidate(TRUE);
 }
